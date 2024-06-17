40a2c443be408f205c7c57248ee4326f0b4d07bc
José F. Rives <jose@digio.es>
2024-06-17 16:51:39 +0200

    fix(sops): Fix separated keys files concatenation

--- git_secrets/scripts/encrypt_yaml.sh
+++ git_secrets/scripts/encrypt_yaml.sh
@@ -3,6 +3,6 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${scriptDir}/.." || exit 1
 
-export SOPS_AGE_RECIPIENTS=$(cat keys/*.txt)
+export SOPS_AGE_RECIPIENTS=$(find . -name 'keys/*.txt' -print0 | xargs -0 sed 's/$/,/' | tr -d '\n')
 exec 3<<< "$(cat $1)"
 sops --encrypt --input-type yaml --output-type yaml --encrypted-regex "^data$|^environment$" --age ${SOPS_AGE_RECIPIENTS} /dev/fd/3
